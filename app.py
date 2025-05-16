1b6562b80af6db4e3e9f3f46d184045c8d71c467
sucovello <sucosta18@gmail.com>
2025-05-16 01:10:59 +0000

    final

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import streamlit as st
 import nbformat as nbf
 
 
-df_vehicles = pd.read_csv('../vehicles.csv')
+df_vehicles = pd.read_csv('./vehicles.csv')
 
 st.header('Veículos à venda')
 
-hist_button = st.button('Criar histograma') # criar um botão
+hist_button = st.button('Histograma') # criar um botão
         
 if hist_button: # se o botão for clicado
             # escrever uma mensagem
@@ -22,14 +22,14 @@ if hist_button: # se o botão for clicado
     st.plotly_chart(fig, use_container_width=True)
 
 
-hist_button = st.button('Criar gráfico de dispersão') # criar um botão
+hist_check = st.checkbox('Gráfico de dispersão') 
         
 if hist_button: # se o botão for clicado
             # escrever uma mensagem
     st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
             
             # criar um histograma
-    fig = px.histogram(df_vehicles, x="price")
+    fig = px.scatter(df_vehicles, x="year", y="price", title="Preço dos veículos por ano")
         
             # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
